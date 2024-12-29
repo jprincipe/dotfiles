af9ec52e0eeafc6cfa461c8b99463ed4cb24772f
Jon Principe <jon@exchangeflo.io>
2024-12-29 18:14:29 -0500

    lazyvim config updates
    added avante support
    updated color scheme to catppuccin frappe

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,16 +1,30 @@
 return {
+  -- { "arcticicestudio/nord-vim" },
   {
     "catppuccin/nvim",
     lazy = false,
     priority = 1000,
     config = function()
+      require("catppuccin").setup({
+        flavour = "frappe", -- latte, frappe, macchiato, mocha
+        styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+          comments = { "italic" }, -- Change the style of comments
+          conditionals = { "italic" },
+          loops = {},
+          functions = {},
+          keywords = { "italic" },
+          strings = {},
+          variables = {},
+          numbers = {},
+          booleans = {},
+          properties = {},
+          types = {},
+          operators = {},
+          -- miscs = {}, -- Uncomment to turn off hard-coded styles
+        },
+      })
+
       vim.cmd.colorscheme("catppuccin-frappe")
     end,
-  },
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "catppuccin-frappe",
-    },
   },
 }
