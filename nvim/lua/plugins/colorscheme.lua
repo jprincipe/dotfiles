ea4fd51d2a832162b7a918785115fb0fa5973aa9
Jon Principe <jon@exchangeflo.io>
2025-01-10 21:30:21 -0500

    updates to nvim config
    update colorscheme to ayu-mirage
    remove elixir-tools in favor of the default lang setup
    setup neotest

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,4 +1,18 @@
 return
+-- Ayu
+{
+  "Shatur/neovim-ayu",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    require("ayu").setup({
+      mirage = true, -- Set to `true` to use `mirage` variant instead of `dark` for dark background.
+      terminal = true, -- Set to `false` to let terminal manage its own colors.
+      overrides = {}, -- A dictionary of group names, each associated with a dictionary of parameters (`bg`, `fg`, `sp` and `style`) and colors in hex.
+    })
+    vim.cmd.colorscheme("ayu")
+  end,
+}
 -- Kanagawa
 -- {
 --   "rebelot/kanagawa.nvim",
@@ -9,15 +23,15 @@ return
 --   end,
 -- }
 --  NORD
-{
-  { "arcticicestudio/nord-vim" },
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "nord",
-    },
-  },
-}
+-- {
+--   { "arcticicestudio/nord-vim" },
+--   {
+--     "LazyVim/LazyVim",
+--     opts = {
+--       colorscheme = "nord",
+--     },
+--   },
+-- }
 --   { "Mofiqul/dracula.nvim" },
 --
 --   -- Configure LazyVim to load dracula
