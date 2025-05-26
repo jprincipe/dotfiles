77f644f54e68ff5d669c6bf285a1a39a6f73da1a
Jon Principe <jon@exchangeflo.io>
2025-05-26 08:30:30 -0400

    lock mason to version 1.8.3 since 2.0 has breaking changes

---
+++ nvim/lua/plugins/mason.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    version = "1.8.3", -- Last version before 2.0
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    version = "v1.32.0",
+  },
+}
