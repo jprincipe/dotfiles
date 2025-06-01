b12e5845141838d6fd757326baa567fae3eedca0
Jon Principe <jon@exchangeflo.io>
2025-06-01 08:27:53 -0400

    remove code-companion since using claude-code instead

--- nvim/lua/plugins/codecompanion.lua
+++ nvim/lua/plugins/codecompanion.lua
@@ -1,30 +1,30 @@
 return {
-  "olimorris/codecompanion.nvim",
-  event = "VeryLazy",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-treesitter/nvim-treesitter",
-  },
-  keys = {
-    { mode = { "v", "n" }, "<Leader>at", "<cmd>CodeCompanionChat Toggle<cr>", desc = "CodeCompainion Chat Toggle" },
-    { mode = { "n", "v" }, "<Leader>aa", "<cmd>CodeCompanionActions<cr>", desc = "CodeCompanion Actions" },
-    { mode = { "v" }, "ga", "<cmd>CodeCompanionChat Add<cr>", desc = "CodeCompanion Chat Add" },
-  },
-  config = function()
-    require("codecompanion").setup({
-      strategies = {
-        chat = {
-          adapter = "anthropic",
-        },
-        -- inline = {
-        --   adapter = "anthropic",
-        -- },
-      },
-    })
-
-    local wk = require("which-key")
-    wk.add({
-      { "<leader>a", group = "CodeCompanion" }, -- group
-    })
-  end,
+  -- "olimorris/codecompanion.nvim",
+  -- event = "VeryLazy",
+  -- dependencies = {
+  --   "nvim-lua/plenary.nvim",
+  --   "nvim-treesitter/nvim-treesitter",
+  -- },
+  -- keys = {
+  --   { mode = { "v", "n" }, "<Leader>at", "<cmd>CodeCompanionChat Toggle<cr>", desc = "CodeCompainion Chat Toggle" },
+  --   { mode = { "n", "v" }, "<Leader>aa", "<cmd>CodeCompanionActions<cr>", desc = "CodeCompanion Actions" },
+  --   { mode = { "v" }, "ga", "<cmd>CodeCompanionChat Add<cr>", desc = "CodeCompanion Chat Add" },
+  -- },
+  -- config = function()
+  --   require("codecompanion").setup({
+  --     strategies = {
+  --       chat = {
+  --         adapter = "anthropic",
+  --       },
+  --       -- inline = {
+  --       --   adapter = "anthropic",
+  --       -- },
+  --     },
+  --   })
+  --
+  --   local wk = require("which-key")
+  --   wk.add({
+  --     { "<leader>a", group = "CodeCompanion" }, -- group
+  --   })
+  -- end,
 }
