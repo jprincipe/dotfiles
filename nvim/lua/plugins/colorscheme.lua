7b6a2b468406bb2e1b2ca7e48e1d8d707b151f45
Jon Principe <jon@exchangeflo.io>
2025-07-16 22:18:06 -0400

    updates to colorscheme and cleaning up unused plugins.  Also moved neo-tree to the right side of the window

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,32 +1,73 @@
 return
+-- sonokai
+-- {
+--   "sainnhe/sonokai",
+--   lazy = false,
+--   priority = 1000,
+--   config = function()
+--     -- Optionally configure and load the colorscheme
+--     -- directly inside the plugin declaration.
+--     vim.g.sonokai_enable_italic = true
+--     vim.cmd.colorscheme("sonokai")
+--   end,
+-- }
+-- Kanagawa Paper
+-- {
+--   "thesimonho/kanagawa-paper.nvim",
+--   lazy = false,
+--   priority = 1000,
+--   config = function()
+--     require("kanagawa-paper").setup({
+--       flavour = "frappe", -- latte, frappe, macchiato, mocha
+--       styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+--         comments = { "italic" }, -- Change the style of comments
+--         conditionals = { "italic" },
+--         loops = {},
+--         functions = {},
+--         keywords = { "italic" },
+--         strings = {},
+--         variables = {},
+--         numbers = {},
+--         booleans = {},
+--         properties = {},
+--         types = {},
+--         operators = {},
+--         -- miscs = {}, -- Uncomment to turn off hard-coded styles
+--       },
+--     })
+--
+--     vim.cmd.colorscheme("kanagawa-paper-canvas")
+--   end,
+--   -- opts = { ... },
+-- }
 -- catppuccin
-{
-  "catppuccin/nvim",
-  lazy = false,
-  priority = 1000,
-  config = function()
-    require("catppuccin").setup({
-      flavour = "frappe", -- latte, frappe, macchiato, mocha
-      styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
-        comments = { "italic" }, -- Change the style of comments
-        conditionals = { "italic" },
-        loops = {},
-        functions = {},
-        keywords = { "italic" },
-        strings = {},
-        variables = {},
-        numbers = {},
-        booleans = {},
-        properties = {},
-        types = {},
-        operators = {},
-        -- miscs = {}, -- Uncomment to turn off hard-coded styles
-      },
-    })
-
-    vim.cmd.colorscheme("catppuccin-frappe")
-  end,
-}
+-- {
+--   "catppuccin/nvim",
+--   lazy = false,
+--   priority = 1000,
+--   config = function()
+--     require("catppuccin").setup({
+--       flavour = "frappe", -- latte, frappe, macchiato, mocha
+--       styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+--         comments = { "italic" }, -- Change the style of comments
+--         conditionals = { "italic" },
+--         loops = {},
+--         functions = {},
+--         keywords = { "italic" },
+--         strings = {},
+--         variables = {},
+--         numbers = {},
+--         booleans = {},
+--         properties = {},
+--         types = {},
+--         operators = {},
+--         -- miscs = {}, -- Uncomment to turn off hard-coded styles
+--       },
+--     })
+--
+--     vim.cmd.colorscheme("catppuccin-frappe")
+--   end,
+-- }
 --  Kanagawa
 -- {
 --   "rebelot/kanagawa.nvim",
@@ -37,41 +78,41 @@ return
 --   end,
 -- }
 -- Nord
--- {
---   "gbprod/nord.nvim",
---   lazy = false,
---   priority = 1000,
---   config = function()
---     require("nord").setup({
---       -- your configuration comes here
---       -- or leave it empty to use the default settings
---       transparent = false, -- Enable this to disable setting the background color
---       terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
---       diff = { mode = "bg" }, -- enables/disables colorful backgrounds when used in diff mode. values : [bg|fg]
---       borders = true, -- Enable the border between verticaly split windows visible
---       errors = { mode = "bg" }, -- Display mode for errors and diagnostics
---       -- values : [bg|fg|none]
---       search = { theme = "vim" }, -- theme for highlighting search results
---       -- values : [vim|vscode]
---       styles = {
---         -- Style to be applied to different syntax groups
---         -- Value is any valid attr-list value for `:help nvim_set_hl`
---         comments = { italic = true },
---         keywords = { italic = true },
---         functions = {},
---         variables = {},
---
---         -- To customize lualine/bufferline
---         bufferline = {
---           current = {},
---           modified = { italic = true },
---         },
---       },
---     })
---
---     vim.cmd.colorscheme("nord")
---   end,
--- }
+{
+  "gbprod/nord.nvim",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    require("nord").setup({
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      transparent = false, -- Enable this to disable setting the background color
+      terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+      diff = { mode = "bg" }, -- enables/disables colorful backgrounds when used in diff mode. values : [bg|fg]
+      borders = true, -- Enable the border between verticaly split windows visible
+      errors = { mode = "bg" }, -- Display mode for errors and diagnostics
+      -- values : [bg|fg|none]
+      search = { theme = "vim" }, -- theme for highlighting search results
+      -- values : [vim|vscode]
+      styles = {
+        -- Style to be applied to different syntax groups
+        -- Value is any valid attr-list value for `:help nvim_set_hl`
+        comments = { italic = true },
+        keywords = { italic = true },
+        functions = {},
+        variables = {},
+
+        -- To customize lualine/bufferline
+        bufferline = {
+          current = {},
+          modified = { italic = true },
+        },
+      },
+    })
+
+    vim.cmd.colorscheme("nord")
+  end,
+}
 -- Tokyonight
 -- {
 --   "folke/tokyonight.nvim",
