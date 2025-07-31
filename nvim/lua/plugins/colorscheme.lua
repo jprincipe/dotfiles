67322e8ebc156a0194afe4cbf7e71c421205da73
Jon Principe <jon@exchangeflo.io>
2025-07-31 09:21:03 -0400

    add new default theme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -77,42 +77,51 @@ return
 --     require("kanagawa").load("wave")
 --   end,
 -- }
--- Nord
+-- Nordic
 {
-  "gbprod/nord.nvim",
+  "AlexvZyl/nordic.nvim",
   lazy = false,
   priority = 1000,
   config = function()
-    require("nord").setup({
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      transparent = false, -- Enable this to disable setting the background color
-      terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
-      diff = { mode = "bg" }, -- enables/disables colorful backgrounds when used in diff mode. values : [bg|fg]
-      borders = true, -- Enable the border between verticaly split windows visible
-      errors = { mode = "bg" }, -- Display mode for errors and diagnostics
-      -- values : [bg|fg|none]
-      search = { theme = "vim" }, -- theme for highlighting search results
-      -- values : [vim|vscode]
-      styles = {
-        -- Style to be applied to different syntax groups
-        -- Value is any valid attr-list value for `:help nvim_set_hl`
-        comments = { italic = true },
-        keywords = { italic = true },
-        functions = {},
-        variables = {},
-
-        -- To customize lualine/bufferline
-        bufferline = {
-          current = {},
-          modified = { italic = true },
-        },
-      },
-    })
-
-    vim.cmd.colorscheme("nord")
+    require("nordic").load()
   end,
 }
+-- Nord
+-- {
+--   "gbprod/nord.nvim",
+--   lazy = false,
+--   priority = 1000,
+--   config = function()
+--     require("nord").setup({
+--       -- your configuration comes here
+--       -- or leave it empty to use the default settings
+--       transparent = false, -- Enable this to disable setting the background color
+--       terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+--       diff = { mode = "bg" }, -- enables/disables colorful backgrounds when used in diff mode. values : [bg|fg]
+--       borders = true, -- Enable the border between verticaly split windows visible
+--       errors = { mode = "bg" }, -- Display mode for errors and diagnostics
+--       -- values : [bg|fg|none]
+--       search = { theme = "vim" }, -- theme for highlighting search results
+--       -- values : [vim|vscode]
+--       styles = {
+--         -- Style to be applied to different syntax groups
+--         -- Value is any valid attr-list value for `:help nvim_set_hl`
+--         comments = { italic = true },
+--         keywords = { italic = true },
+--         functions = {},
+--         variables = {},
+--
+--         -- To customize lualine/bufferline
+--         bufferline = {
+--           current = {},
+--           modified = { italic = true },
+--         },
+--       },
+--     })
+--
+--     vim.cmd.colorscheme("nord")
+--   end,
+-- }
 -- Tokyonight
 -- {
 --   "folke/tokyonight.nvim",
