4f37f4c55100762b2d32b0b97036c9731483d898
Jon Principe <jon@exchangeflo.io>
2025-12-31 12:17:21 -0400

    convert from mini.files to oil/neo-tree

---
+++ nvim/lua/plugins/oil.lua
@@ -0,0 +1,9 @@
+return {
+  "stevearc/oil.nvim",
+  dependencies = { "nvim-mini/mini.icons" },
+  lazy = false,
+  keys = {
+    { "-", "<cmd>Oil<cr>", desc = "Open parent directory" },
+  },
+  opts = {},
+}
