4d7f386f4d3de2e90cc8ac870139b4db643c3ff6
Jon Principe <jon@exchangeflo.io>
2026-01-01 21:26:55 -0400

    added <leader>, keymap

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -249,6 +249,7 @@ return {
     { "<leader>fh", function() MiniPick.builtin.help() end, desc = "Help tags" },
     { "<leader>fr", function() MiniPick.builtin.resume() end, desc = "Resume picker" },
     { "<leader><leader>", function() MiniPick.builtin.files() end, desc = "Find files" },
+    { "<leader>,", function() MiniPick.builtin.buffers() end, desc = "Find buffers" },
     -- Bufremove
     { "<leader>bd", function() MiniBufremove.delete() end, desc = "Delete buffer" },
     { "<leader>bw", function() MiniBufremove.wipeout() end, desc = "Wipeout buffer" },
