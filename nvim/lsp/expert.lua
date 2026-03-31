588be906a39b038a6be647f619536534c4900901
Jon Principe <jon@exchangeflo.io>
2026-03-31 10:24:21 -0400

    update elixir expert version

--- nvim/lsp/expert.lua
+++ nvim/lsp/expert.lua
@@ -1,4 +1,4 @@
--- curl -L https://github.com/elixir-lang/expert/releases/download/v0.1.0-rc.6/expert_darwin_amd64 -o ~/.local/bin/expert && chmod +x ~/.local/bin/expert
+-- curl -L https://github.com/elixir-lang/expert/releases/download/v0.1.0/expert_darwin_arm64 -o ~/.local/bin/expert && chmod +x ~/.local/bin/expert
 return {
   cmd = { vim.fn.expand('~/.local/bin/expert'), '--stdio' },
   filetypes = { 'elixir', 'eelixir', 'heex', 'surface' },
